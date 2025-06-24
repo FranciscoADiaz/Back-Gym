11f802f71b60d58634542ec0acbf868905535e27
Diaz-Francisco-Ariel <Arieldiazx@outlook.com.ar>
2025-06-24 04:30:00 -0300

    registro y login

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,4 +1,12 @@
 const {Router} = require('express');
 const router = Router();    
+const { registroUsuario, inicioSesionUsuario, habilitarDeshabilitarUsuario } = require("../controllers/usuarios.controllers");
+
+router.post("/register", registroUsuario);
+router.post("/login", inicioSesionUsuario);
+router.put("/enableDisable/:id", habilitarDeshabilitarUsuario);
+
+
 
 module.exports = router;
+
