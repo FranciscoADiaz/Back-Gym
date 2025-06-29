88ad9081d47bdb95a81f638b524732a26c00643a
Diaz-Francisco-Ariel <Arieldiazx@outlook.com.ar>
2025-06-29 13:50:49 -0300

    reserva de clases v1

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,9 +3,11 @@ const router = Router();
 
 const usuariosRoutes = require("./usuarios.routes.js");
 const productosRoutes = require("./productos.routes.js");
+const reservasRoutes = require("./reserva.routes.js");
 
 router.use("/usuarios", usuariosRoutes);
 router.use("/productos", productosRoutes);
+router.use("/reservar", reservasRoutes);
 
 
 module.exports = router; 
