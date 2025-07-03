c1e2decd5bd4847198bb13cda54eb6ccba2c8b01
Francisco Ariel Diaz <123221707+FranciscoADiaz@users.noreply.github.com>
2025-07-03 16:42:46 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,22 +4,28 @@ require("./DB/config.db.js");
 
 const morgan = require("morgan");
 
-
 const app = express();
 const puerto = process.env.PORT || 3005;
 
 // Middlewares
-app.use(cors({origin:"https://front-gym-rho.vercel.app"}));
+
+app.use(cors({
+  origin: "https://front-gym-rho.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(morgan("dev"));
 
 // Iniciar servidor
 app.listen(puerto, () => {
-  console.log("Servidor funcionando en el puerto", puerto);
+console.log("Servidor funcionando en el puerto", puerto);
 });
-
 
 //Rutas
 app.use("/api", require("./routes/index.routes"));
 
 module.exports = app;
+
