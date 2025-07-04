8159a5a4eaca086e8c770943883b8d52cb0a21cc
Francisco Ariel Diaz <123221707+FranciscoADiaz@users.noreply.github.com>
2025-07-03 23:55:27 -0300

    Update messages.helpers.js

--- src/helpers/messages.helpers.js
+++ src/helpers/messages.helpers.js
@@ -2,10 +2,10 @@ const transporter = require("./nodemailer.helpers");
 
 const registroExitoso = async (emailUsuario, nombreUsuario) => {
   const info = await transporter.sendMail({
-    from: `"Tucu-Gym" <${process.env.GMAIL_USER}>`,
+    from: `"Tucu-Gym" <${process.env.MAILGUN_USER}>`,  // ✅ CAMBIADO a MAILGUN_USER
     to: `${emailUsuario}`,
     subject: `Bienvenido  ${nombreUsuario} ✔`,
-    text: "En breve podras iniciar sesión",
+    text: "En breve podrás iniciar sesión",
     html: `
     <img src="https://png.pngtree.com/png-clipart/20231015/original/pngtree-man-character-training-at-the-gym-vector-illustration-png-image_13302900.png" alt="gif">
     <h1>Gracias por registrarte en nuestra web </h1> ${nombreUsuario}
@@ -20,14 +20,13 @@ const registroExitoso = async (emailUsuario, nombreUsuario) => {
 
 const recoveryPassEmail = async (emailUsuario, token) => {
   const info = await transporter.sendMail({
-    from: `"Tucu-Gym" <${process.env.GMAIL_USER}>`,
+    from: `"Tucu-Gym" <${process.env.MAILGUN_USER}>`,  // ✅ CAMBIADO a MAILGUN_USER
     to: `${emailUsuario}`,
     subject: `Recuperación de contraseña`,
     html: `
     <img src="https://media.tenor.com/FWMGiswEeZUAAAAM/password.gif" alt="gif">
     <h1>Gracias por ser parte de esta comunidad</h1>
-    <a href="${process.env.URL_FRONT}/recoveryPass?token=${token}">Ir a la pagina</a>
-    
+    <a href="${process.env.URL_FRONT}/recoveryPass?token=${token}">Ir a la página</a>
     `,
   });
   return {
