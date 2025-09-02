f57427dc51ac44122f54ec31cff4db30bb1c739a
Diaz-Francisco-Ariel <Arieldiazx@outlook.com.ar>
2025-09-02 15:38:12 -0300

    DB: corrección archivo conf db

--- src/config/mercadopago.config.js
+++ src/config/mercadopago.config.js
@@ -1,6 +1,5 @@
 const { MercadoPagoConfig, Preference } = require("mercadopago");
 
-// Configurar MercadoPago con tu Access Token
 const client = new MercadoPagoConfig({
   accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN,
 });
